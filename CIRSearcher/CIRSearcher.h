06b23578ad601324414ca450d19923b6c64bb449
Kam <kamtoto.au@gmail.com>
2021-07-08 23:08:05 +0800

    Initial Commit

---
+++ CIRSearcher/CIRSearcher.h
@@ -0,0 +1,12 @@
+//
+//  CIRSearcher.h
+//  CIRSearcher
+//
+//  Created by Kam on 2021/7/8.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CIRSearcher : NSObject
+
+@end
---
+++ CIRSearcher/CIRSearcher.m
@@ -0,0 +1,12 @@
+//
+//  CIRSearcher.m
+//  CIRSearcher
+//
+//  Created by Kam on 2021/7/8.
+//
+
+#import "CIRSearcher.h"
+
+@implementation CIRSearcher
+
+@end
